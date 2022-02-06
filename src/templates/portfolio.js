58fe68be5fd23b49b6fd9f36efd2d45ad9354a9b
stevemzen <stevemzen@users.noreply.github.com>
2022-02-05 19:25:32 -0500

    style portfolio items on home page

--- src/templates/portfolio.js
+++ src/templates/portfolio.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Layout from "../components/layout";
+import styled from "styled-components";
 
-const Portfolio = ({ pageContext }) => {
-  return (
-    <div>
-      <h1>{pageContext.title}</h1>
-    </div>
-  );
-};
+const FeaturedImage = styled.img`
+  max-width: 300px;
+  margin: 16px 0;
+`;
+
+const Portfolio = ({ pageContext }) => (
+  <Layout>
+    <h1>{pageContext.title}</h1>
+    <FeaturedImage src={pageContext.featured_media.source_url} />
+    <div dangerouslySetInnerHTML={{ __html: pageContext.content }} />
+  </Layout>
+);
 
 export default Portfolio;
