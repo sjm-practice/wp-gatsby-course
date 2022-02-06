940d999c4bcdc07e497e07a0c7aedc5b3603f02e
stevemzen <stevemzen@users.noreply.github.com>
2022-02-06 13:07:51 -0500

    add blog page links

--- src/templates/blogPostList.js
+++ src/templates/blogPostList.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Layout from "../components/layout";
+import { Link } from "gatsby";
 import styled from "styled-components";
 
 const BlogPostList = ({ pageContext }) => (
   <Layout>
     {pageContext.posts.map(post => (
-      <div>
+      <div key={post.node.wordpress_id}>
         <h3 dangerouslySetInnerHTML={{ __html: post.node.title }} />
         <p dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
+      </div>
+    ))}
+    {Array.from({ length: pageContext.numberOfPages }).map((page, index) => (
+      <div key={index}>
+        <Link to={index === 0 ? `/blog/` : `/blog/${index + 1}/`}>
+          {index + 1}
+        </Link>
       </div>
     ))}
   </Layout>
